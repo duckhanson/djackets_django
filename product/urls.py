47a09c294e758eae0c1d803c9046b1b86b67258a
Chia-Han Lu <ducklyu0301@gmail.com>
2022-05-06 18:20:28 -0500

    add product detail requests @views&urls

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,8 @@
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.contrib import admin
 from django.urls import path, include
 
 from product import views
 
 urlpatterns = [
     path('lastest-products/', views.LatestProductsList.as_view()),
+    path('product/<slug:category_slug>/<slug:product_slug>', views.ProductDetails().as_view()),
 ]
